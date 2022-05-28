17f136df6a1052b1d3cea5cc840d81784af3014e
WallnussJonas <jonas.werner.nov1999@gmail.com>
2022-05-28 11:58:46 +0200

    Passenger Counter App V1

---
+++ index.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+}
+
+function decrement() {
+    count -= 1;
+    countEl.innerText = count;
+}
+
+function reset() {
+    count = 0;
+    countEl.innerText = count;
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    console.log(count)
+}
